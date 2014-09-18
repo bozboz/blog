9c205c6985154cb35f4a4d05d0d0f4689b60abc6
Dan Bower <dan.bower88@gmail.com>
2014-09-18 17:21:17 +0100

    Ensure each BlogCategory "name" is unique

--- src/Bozboz/Blog/Validators/BlogCategoryValidator.php
+++ src/Bozboz/Blog/Validators/BlogCategoryValidator.php
@@ -5,7 +5,7 @@ use Bozboz\Admin\Services\Validators\Validator;
 class BlogCategoryValidator extends Validator
 {
 	protected $rules = [
-		'name' => 'required|min:3|max:255',
+		'name' => 'required|min:3|max:255|unique:blog_categories',
 		'status' => 'required'
 	];
 }
