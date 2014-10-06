81930a8ece1def86b18cdd8edcbc5805fba634c5
Dan Bower <dan.bower88@gmail.com>
2014-10-06 15:53:59 +0100

    Add "AddThis" social media buttons to BlogPost detail view

--- src/views/post/detail.blade.php
+++ src/views/post/detail.blade.php
@@ -1,3 +1,7 @@
 <h2>{{ $blogPost->title }}</h2>
 
 {{ $blogPost->content  }}
+
+<div class="addthis_sharing_toolbox"></div>
+
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-51dd56d21dea2c34" async></script>
