f3e9201cbb8c4b1be8f68bd7590971fbb2d44ccc
Joao Augusto <joao@bozboz.co.uk>
2015-06-12 15:41:28 +0100

    Making Blog name a config value

--- src/config/config.php
+++ src/config/config.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+	'url_prefix' => 'blog',
 	'blog_count_on_index' => 10, //Number of blog posts to display on the "blog.index" route
 	'sticky_posts_enabled' => true, //Ability to tag a BlogPost to appear first within a BlogCategory
 ];
--- src/routes.php
+++ src/routes.php
@@ -5,11 +5,24 @@ Route::group(['namespace' => 'Bozboz\Blog\Controllers'], function()
 	Route::resource('admin/blog/posts', 'BlogPostAdminController');
 	Route::resource('admin/blog/categories', 'BlogCategoryAdminController');
 
-	Route::get('blog', ['as' => 'blog.index', 'uses' => 'BlogPostController@getIndex']);
-	Route::get('blog/archive', ['as' => 'blog.archive', 'uses' => 'BlogPostController@getArchive']);
-	Route::get('blog/{slug}', ['as' => 'blog.detail', 'uses' => 'BlogPostController@getDetail']);
-	Route::get('blog/category/{slug}', [
-		'as' => 'blog-category.listing',
-		'uses' => 'BlogCategoryController@getBlogListing'
-	]);
+	Route::group(['prefix' => Config::get('blog::url_prefix')], function()
+	{
+		Route::get('/', [
+			'as' => 'blog.index',
+			'uses' => 'BlogPostController@getIndex'
+		]);
+		Route::get('archive', [
+			'as' => 'blog.archive',
+			'uses' => 'BlogPostController@getArchive'
+		]);
+		Route::get('{slug}', [
+			'as' => 'blog.detail',
+			'uses' => 'BlogPostController@getDetail'
+		]);
+		Route::get('category/{slug}', [
+			'as' => 'blog-category.listing',
+			'uses' => 'BlogCategoryController@getBlogListing'
+		]);
+	});
+
 });
