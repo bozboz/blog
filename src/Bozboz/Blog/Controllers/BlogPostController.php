b511eae4228d7057ed28541ccadfcde863a90d4a
Ceawlin Jones <ceawlin@bozboz.co.uk>
2015-07-23 15:57:41 +0100

    Update blog - set blog post order by post date

--- src/Bozboz/Blog/Controllers/BlogPostController.php
+++ src/Bozboz/Blog/Controllers/BlogPostController.php
@@ -11,7 +11,7 @@ class BlogPostController extends Controller
 	public function getIndex()
 	{
 		$blogCount = Config::get('blog::blog_count_on_index');
-		$blogPosts = BlogPost::active()->take($blogCount)->latest()->simplePaginate(12);
+		$blogPosts = BlogPost::active()->take($blogCount)->orderBy('post_date', 'DESC')->simplePaginate(12);
 		if ($blogPosts->isEmpty()) {
 			$response = View::make('blog::post.empty');
 		} else {
@@ -36,7 +36,7 @@ class BlogPostController extends Controller
 
 	public function getArchive()
 	{
-		$blogPosts = BlogPost::active()->latest()->simplePaginate(12);
+		$blogPosts = BlogPost::active()->orderBy('post_date', 'DESC')->simplePaginate(12);
 
 		return View::make('blog::post.archive')->with('blogPosts', $blogPosts);
 	}
