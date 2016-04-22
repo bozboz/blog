51e6506666f2957ea3dec4dc48b965978039ee1b
Sam King <sdking02@gmail.com>
2016-04-22 13:04:01 +0100

    Fix 500 response and ordering

--- changelog.md
+++ changelog.md
@@ -1,9 +1,13 @@
-# Version v0.2.0 (2015-05-06)
+# Version 0.2.1 (2016-04-22)
+-   Fix: Missing blog posts now produce 404 rather than 500 response
+-   Fix: use post date for ordering, not created_at
+
+# Version 0.2.0 (2015-05-06)
 -   Add youtube_url field to BlogPost
 -   Add DateTimeField to post_date attribute
 -   Add DynamicSlugField
 -   Updated for bozboz/admin v1.0.0
 
-# Version v0.1.1 (2015-01-29)
+# Version 0.1.1 (2015-01-29)
 
 -   Tie bozboz/admin dependency to ~0.4
--- src/Bozboz/Blog/Controllers/BlogPostController.php
+++ src/Bozboz/Blog/Controllers/BlogPostController.php
@@ -5,13 +5,14 @@ use Config;
 use Controller;
 use Bozboz\Blog\Models\BlogPost;
 use Bozboz\Blog\Models\BlogStatus;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class BlogPostController extends Controller
 {
 	public function getIndex()
 	{
 		$blogCount = Config::get('blog::blog_count_on_index');
-		$blogPosts = BlogPost::active()->take($blogCount)->latest()->get();
+		$blogPosts = BlogPost::active()->take($blogCount)->latest('post_date')->get();
 		if ($blogPosts->isEmpty()) {
 			$response = View::make('blog::post.empty');
 		} else {
@@ -27,7 +28,9 @@ class BlogPostController extends Controller
 	{
 		$blogPost = BlogPost::where('slug', '=', $slug)
 					->active()
-					->firstOrFail();
+					->first();
+
+		if ( ! $blogPost) throw new NotFoundHttpException;
 
 		return View::make('blog::post.detail', [
 			'blogPost' => $blogPost
